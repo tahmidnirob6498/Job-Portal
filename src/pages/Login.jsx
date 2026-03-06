ba213ba869adb4341bc8720a8ac36f51af229d82
htamid <htahmid6498@gmail.com>
2026-03-06 06:07:17 +0600

    added a login and signup form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
+  const handleLogin=(e)=>{
+ e.preventDefault()
+ const email=e.target.email.value
+ const password=e.target.password.value
+ console.log(email,password)
+
+  }
+
     return (
-        <div className="card bg-base-100 w-full mt-15 mx-auto max-w-sm shrink-0 border shadow-2xl">
+        <div className="card bg-base-100 w-full mt-15 mx-auto max-w-sm border shrink-0 border-amber-300 shadow-2xl">
      
       <div className="card-body">
-        <h1 className='text-center text-3xl font-bold'>Login</h1>
-        <fieldset className="fieldset">
+        <h1 className='text-center text-2xl font-semibold'>Login</h1>
+        <form onSubmit={handleLogin} className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" name='email' className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" name='password' className="input" placeholder="Password" />
           <div><a className="link link-hover">Forgot password?</a></div>
           <button className="btn bg-amber-400 mt-4">Login</button>
-        </fieldset>
+          <p className='text-center'>Don't have an account?<Link className='text-amber-500' to='/signup'>Create account</Link></p>
+        </form>
       </div>
     </div>
     );
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Signup = () => {
+    const handleSignUp=(e)=>{
+  e.prevntDefault()
+  const name=e.target.name.value
+  const email=e.target.email.value
+  const photo=e.target.photo.value
+  const password=e.target.password.value
+  console.log(name,password,email,photo)
+    }
     return (
-        <div>
-            
-        </div>
+         <div className="card bg-base-100 w-full mt-15 mx-auto max-w-sm border shrink-0 border-amber-300 shadow-2xl">
+     
+      <div className="card-body">
+        <h1 className='text-center text-2xl font-semibold'>Create Your Account</h1>
+        <form onSubmit={handleSignUp} className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" name='name' className="input" placeholder="Name" />
+          <label className="label">Email</label>
+          <input type="email" name='email' className="input" placeholder="Email" />
+          <label className="label">Photo url</label>
+          <input type="text" name='photo' className="input" placeholder=" Your photo url" />
+          <label className="label">Password</label>
+          <input type="password" name='password' className="input" placeholder="Password" />
+          <button className="btn bg-amber-400 mt-4">Create account</button>
+          <p className='text-center'>Already have an account?<Link className='text-amber-500' to='/login'>Login</Link></p>
+        </form>
+      </div>
+    </div>
     );
 };
 
